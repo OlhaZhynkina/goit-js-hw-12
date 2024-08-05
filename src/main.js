35d91cd621b2d973af91a6cdbd2fac818506d1d7
Olha <olyasavvz@gmail.com>
2024-08-05 17:14:33 +0300

    done

--- src/main.js
+++ src/main.js
@@ -2,24 +2,36 @@ import iziToast from 'izitoast';
 import SimpleLightbox from 'simplelightbox';
 import 'izitoast/dist/css/iziToast.min.css';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { getPhotos } from './js/pixabay-api';
+import { getPhotos, perPage } from './js/pixabay-api';
 import { imagesMarkup } from './js/render-functions';
 import iconError from './img/error.svg';
+import iconCaution from './img/caution.svg';
+import iconSuccess from './img/success.svg';
 
 const formElement = document.querySelector('.js-form');
 const ulElement = document.querySelector('.js-gallery-list');
 const loader = document.querySelector('.loader');
+const loadBtn = document.querySelector('.load-btn');
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+let searchQuery;
+let currentPage = 1;
+let numberOfPages = null;
 
 formElement.addEventListener('submit', onSearchPhotos);
-//функція, яка потрібна, щоб при натисканні людиною кнопки:
+loadBtn.addEventListener('click', OnLoadMore);
 
-function onSearchPhotos(event) {
+async function onSearchPhotos(event) {
   event.preventDefault();
-  ulElement.innerHTML = ''; // Очищує розмітку бо потім я використаю замість innerHTML на 55 рядку insertAdjacentHTML() і через в мене перестане очищуватись розмітка
-  // 1) Отримати дані з форми, які ввела людина (введене значення)
-  const formData = new FormData(formElement); // Отримуємо екземпляр класу форми new FormData (це потрібно для того, щоб використати метод get)
-  const searchQuery = formData.get('search').trim(); // Дістаємо дані, які ввела людина, щоб це зробити потрібно діставати їх по атрибуту name інпуту. А потім методом trim прибираємо пробіли
-  // роблю перевірку, чи не відправили мені пусте поле
+  ulElement.innerHTML = '';
+  currentPage = 1;
+  loadBtn.classList.add('hidden');
+
+  const formData = new FormData(formElement);
+  searchQuery = formData.get('search').trim();
+
   if (searchQuery === '') {
     return iziToast.error({
       iconUrl: iconError,
@@ -31,40 +43,95 @@ function onSearchPhotos(event) {
       message: 'The field cannot be empty!',
     });
   }
-  // Перед запитом на сервер, якщо я виуористовую лоадер його потрібно показати. Це робиться за допомогою видалення класу
-  loader.classList.remove('hidden');
-  // Викликаємо функцію запиту на сервер, яка була написана у pixabay-api і передаємо пошуковий запит (те, що ввела людина, дані, що ми раніше дістали)
-  getPhotos(searchQuery)
-    // Оброблюємо успішну відповідь, яка приходить з серверу (тобто мається на увазі, що все ок і в нас на сторінці показуються картинки)
-    .then(response => {
-      // перевіряємо чи нам не прийшла з серверу пуста відповідь і чи є дані за таким пошуком
-      if (response.hits.length === 0) {
-        return iziToast.error({
-          iconUrl: iconError,
-          title: 'Error',
-          titleColor: '#fff',
-          messageColor: '#fff',
-          backgroundColor: '#ef4040',
-          position: 'topRight',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-      }
 
-      // Це функці рендеру. Ми відмальовуємо отримані дані з серверу на сторінці (тобто картинки та дані під ними)
-      ulElement.innerHTML = imagesMarkup(response.hits); // hits - я дивлюсь на вкладці networks в інструментах розробника (може бути різні слово в залежності від бібіліотеки)
-      // Використовуємо бібліотеку, щоб натискати на зображення та відкривати модальне вікно
-      const lightbox = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
+  loader.classList.remove('hidden');
+
+  try {
+    const result = await getPhotos(searchQuery, currentPage);
+
+    if (result.hits.length === 0) {
+      return iziToast.error({
+        iconUrl: iconError,
+        title: 'Error',
+        titleColor: '#fff',
+        messageColor: '#fff',
+        backgroundColor: '#ef4040',
+        position: 'topRight',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
       });
-      lightbox.refresh(); // Нам завантажується по 20 зображень. Коли ми відкриваємо модалку, то вона запам'ятовує та в куточку пише, що зображень лише 20. А припустимо, що в нас є кнопка завантажити більше. Без цього тетому в модалці так і лишиться лише 20 зображень, а наступні не відкриватимуться у модальному вікні. Цей метод бібліотеки для того, щоб оновлювати інформацію і в модалці завантажувалось більше зображень
-    })
-    .catch(error => {
-      console.log(error);
-    })
-    .finally(() => {
-      loader.classList.add('hidden');
-      event.target.reset();
-    });
-} // очищаємо поля та приховуємо лоадер (цей блок виконається в будь-якому випадку і при помилці і при успішному)
+    }
+
+    if (result.totalHits !== 0) {
+      iziToast.success({
+        iconUrl: iconSuccess,
+        title: 'OK',
+        titleColor: '#fff',
+        messageColor: '#fff',
+        backgroundColor: '#59a10d',
+        position: 'topRight',
+        message: `We found ${result.totalHits} images for your request!`,
+      });
+    }
+
+    if (result.totalHits > 15) {
+      loadBtn.classList.remove('hidden');
+    }
+
+    numberOfPages = Math.ceil(result.totalHits / perPage);
+
+    ulElement.innerHTML = imagesMarkup(result.hits);
+
+    lightbox.refresh();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loader.classList.add('hidden');
+    event.target.reset();
+  }
+}
+
+async function OnLoadMore() {
+  loadBtn.classList.add('hidden');
+  currentPage += 1;
+
+  loader.classList.remove('hidden');
+
+  try {
+    const result = await getPhotos(searchQuery, currentPage);
+    ulElement.insertAdjacentHTML('beforeend', imagesMarkup(result.hits));
+
+    lightbox.refresh();
+
+    if (currentPage < numberOfPages) {
+      loadBtn.classList.remove('hidden');
+    }
+
+    if (currentPage === numberOfPages) {
+      iziToast.warning({
+        iconUrl: iconCaution,
+        title: 'WARNING',
+        titleColor: '#fff',
+        messageColor: '#fff',
+        backgroundColor: '#09f',
+        position: 'topRight',
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loader.classList.add('hidden');
+    scrollPage();
+  }
+}
+
+function scrollPage() {
+  const pictureHeight =
+    ulElement.firstElementChild.getBoundingClientRect().height;
+
+  window.scrollBy({
+    top: pictureHeight * 2,
+    behavior: 'smooth',
+  });
+}
